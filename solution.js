e7a5db3c08159b7b0165150952290ab6d09d0123
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-07 11:55:08 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,22 @@
+euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+console.log(euroCities);
+secondCity = euroCities[1];
+console.log(secondCity);
 
+euroCities[0]="Berlin";
+console.log(euroCities);
+
+console.log(euroCities.length);
+euroCities.pop();
+console.log(euroCities);
+
+euroCities.push("Budapest");
+console.log(euroCities);
+
+asianCities = ["Shanghai", "Beijing", "Shenzhen", "Guangzhou", "Chengdu"];
+console.log(asianCities);
+
+ worldCities = [...euroCities, ...asianCities];
+ console.log(worldCities);
+
+console.log(worldCities.reverse());
